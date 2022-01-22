4a08816c55570c39b1ba08727ecbeff0234e3159
Zac <ztaylor555@gmail.com>
2022-01-22 14:39:09 -0500

    Fix links, add ticket text hero

--- template-parts/modules/hero-full-fp.php
+++ template-parts/modules/hero-full-fp.php
@@ -12,7 +12,14 @@ $tickets_link = get_field('tickets_link');
 
 <?php if($events->have_posts() ) {  //If events has posts?>
 
-<?php while($events->have_posts()): $events->the_post(); $thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full'); ?>
+<?php while($events->have_posts()): $events->the_post(); $thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full'); 
+
+    $tickets_link = get_field('tickets_link');
+    $tickets_text = get_field('tickets_text');
+
+    if(empty($tickets_text)){
+        $tickets_text = 'Get Tickets!';
+    } ?>
 
 <section class="hero hero--full margin-none">
     <div class="hero__image" style="background-image:url(<?php echo $thumbnail['0']; ?>);"></div>
@@ -27,8 +34,8 @@ $tickets_link = get_field('tickets_link');
             <?php the_field('event_date'); ?>
         </div>
         <div class="hero__cta">
-            <?php if( get_field('tickets_link') ): ?>
-                <a target="_blank" href="<?php echo $tickets_link; ?>" class="thoroughbreds-button primary small animated flipInX slide2_button1 delay3">Get tickets!</a>
+            <?php if( !empty($tickets_link ) ): ?>
+                <a target="_blank" href="<?php echo $tickets_link; ?>" class="thoroughbreds-button primary small animated flipInX slide2_button1 delay3"><?php echo $tickets_text; ?></a>
             <?php endif; ?>
 
             <a href="<?php the_permalink(); ?>" class="thoroughbreds-button secondary small animated flipInX slide2_button1 delay3">More Info</a>
