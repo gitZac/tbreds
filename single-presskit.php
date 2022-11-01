fc2fbad305d513ce2fdd06a89c27f13d05562493
Zac <ztaylor555@gmail.com>
2022-11-01 17:29:55 -0400

    Finish kit

--- single-presskit.php
+++ single-presskit.php
@@ -1,1 +1,25 @@
-<h1>From single presskti</h1>
+<?php
+/**
+ * The template for displaying all pages.
+ * This is the single template page for the presskit.
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ * @package thoroughbreds
+ */
+get_header();
+?>
+
+<div id="primary" class="content-area">
+    <main id="main" class="site-main thoroughbreds-page" role="main">
+        <div class="container">
+            <?php while (have_posts()) : the_post(); ?>
+                <?php get_template_part('template-parts/content', 'plainheader'); ?>
+                <?php the_field('kit_post_content'); ?>
+                <?php get_template_part('template-parts/content', 'aboutcta'); ?>
+            <?php endwhile; // End of the loop. ?>
+        </div>
+
+    </main><!-- #main -->
+
+</div><!-- #primary -->
+
+<?php get_footer(); ?>
